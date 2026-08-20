dbe76c03783c2a58a3c6df5546e022a1214f4ae8
Sam <sam-henderson@outlook.com>
2026-08-20 10:57:48 +0300

    Added exercise 3

---
+++ Module3/exercise3.py
@@ -0,0 +1,10 @@
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+perimeter = 2 * (length + width)
+area = length * width
+
+
+print("The perimeter of the rectangle is " + str(perimeter))
+print("The area of the rectangle is " + str(area))
+
