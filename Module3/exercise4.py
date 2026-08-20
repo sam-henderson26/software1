03ae1ec62d71bd5914eff11a1bd584d4193cbf98
Sam <sam-henderson@outlook.com>
2026-08-20 11:21:48 +0300

    Added exercise 4

---
+++ Module3/exercise4.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+
+sum_of_numbers = (num1 + num2 + num3)
+product_of_numbers = (num1 * num2 * num3)
+average_of_numbers = ((num1 + num2 + num3) / 3)
+
+print("The sum of the numbers: " + str(sum_of_numbers))
+print("The product of the numbers: " + str(product_of_numbers))
+print("The average of the numbers: " + str(average_of_numbers))
